540855c8446ad30e7b562e1c0393074834b9e25c
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-24 18:48:04 +0000

    added

--- Task 7.py
+++ Task 7.py
@@ -8,16 +8,37 @@
 #The purpose of this program is to take the student's information and then prompt the student to enter in the course codes of the programs 
 #they want to register for. The program will then display they information and courses they entered.
 
-student = []
+student = []   #defining the variable as a list
 
-firstName = input('Please enter your firstname: ')
-
-lastName = input('Please enter your last name: ')
-
-studentNumber = input('Please enter your student number: ')
-
+firstName = input('Please enter your firstname: ').capitalize().strip()
 student.append(firstName)
-student.append(lastName)
-student.append(studentNumber)
+#asking for the user's first name and adding it to the list
 
-print(student)
+lastName = input('Please enter your last name: ').capitalize().strip()
+student.append(lastName)
+#asking for last name and adding to list
+
+studentNumber = int(input('Please enter your student number: '))
+student.append(studentNumber)
+#asking for student number and adding to list
+
+course = input('Please enter the programs you wish to register '
+               'for (put a comma between each code): ').upper().strip()
+#asking for the course codes from user and adding to a new list
+
+courseList = course.split(',') #splitting string into list
+
+print('') #adding blank space
+
+print('Student Information: \n')
+
+print(*student, sep='\n') #printing student information
+
+print('') #blank space
+
+print('Registered Courses: \n')
+
+for i in courseList:
+    print(i)
+#printing course list
+
